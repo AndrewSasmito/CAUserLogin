ac76f9103cb562ef06265f2a0f8abd940c0ddb4c
AndrewSasmito <asasmito1920@gmail.com>
2024-10-26 23:41:39 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,16 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name with the name to set
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return String of the user
+     */
+    String getCurrentUser();
+
 }
